be3d78acd3298e3e85c72fcc9f26eac602ab1c2b
Nailton Oliveira <nailton.me91@gmail.com>
2024-07-24 21:27:14 -0300

    fix: vitest resolve path to imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -12,6 +13,11 @@ export default defineConfig({
         ...coverageConfigDefaults.exclude,
         "**/{next,postcss,tailwind}.config.?(c|m)[jt]s",
       ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
